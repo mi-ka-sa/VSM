b9e5ffbf610a1865639071f172464ccff66156a9
mikasa <logan8601@gmail.com>
2023-03-05 22:49:03 +0100

    set score for title

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($) {
+    $('#score-div').on('click', '.fa', function(e) {
+        const id = $(this).data('tid');
+        const value = $(this).data('value');
+        $.ajax({
+            url: 'score/add',
+            type: 'POST',
+            data: { id: id, value: value },
+            success: function(res) {
+                location.reload();
+            },
+            error: function() {
+                alert('Error: not response from Controller');
+            },
+        });
+    });
+
+    $('#score-div').on('click', '.delete-score', function(e) {
+        const id = $(this).data('tid');
+        $.ajax({
+            url: 'score/delete',
+            type: 'POST',
+            data: { id: id },
+            success: function(res) {
+                location.reload();
+            },
+            error: function() {
+                alert('Error: not response from Controller');
+            },
+        });
+    });
+});
--- src/Handlers/Manga.php
+++ src/Handlers/Manga.php
@@ -8,61 +8,24 @@ use App\App;
 
 class Manga extends App
 {
+    use Addition;
+    
     public function show($param)
     {
         extract($param);
-        if ($slug == 'catalog')
-        {
-            $this->catalogShow();
-        } else {
-            $sth = $this->db->prepare("SELECT t.*, tt.*, g.name AS genre FROM titles t
-                                    JOIN titles_add_text tt
-                                    ON t.id = tt.title_id
-                                    JOIN titles_genres tg
-                                    ON t.id = tg.id_title
-                                    JOIN genres g
-                                    ON g.id = tg.id_genre
-                                    WHERE t.slug = :slug AND t.display = 1");
-            $sth->execute(['slug' => $slug]);
-            $res = $sth->fetchAll();
 
-            if (empty($res)) {
-                debug('Page not found', 1);
-            }
+        $item = $this->getTitle($slug, 'titles_add_text', 'type', 'status', 'volumes', 'chapters', 'author_story', 'author_art');
+        $item['score'] = $this->getScore($item['id']);
+        $data['title'] = $item;
 
-            foreach ($res as $item) {
-                $genres[] = $item['genre'];
-            }
-            $data['item'] = array_shift($res);
-            $data['item']['genre'] = $genres;
-
-            $this->renderer->display('Manga', $data);
-        }
-    
+        $this->renderer->display('Title', $data);
     }
 
-    public function catalogShow ()
+    public function catalogShow()
     {
-        $sth = $this->db->query("SELECT t.id, t.name, t.name_alt, t.img, t.type AS 'type', t.new, t.slug,
-                                        tt.type AS type_type, tt.chapters, 
-                                        GROUP_CONCAT(g.name SEPARATOR ' / ') AS genre 
-                                    FROM titles t
-                                    JOIN titles_add_text tt
-                                    ON t.id = tt.title_id
-                                    JOIN titles_genres tg
-                                    ON t.id = tg.id_title
-                                    JOIN genres g
-                                    ON g.id = tg.id_genre
-                                    WHERE t.display = 1
-                                    GROUP BY tg.id_title");
-        $res = $sth->fetchAll();
+        $items = $this->getAllTitles('titles_add_text', 'type',  'chapters');
+        $data['db_data'] = $items;
 
-        if (empty($res)) {
-            debug('No items', 1);
-        }
-
-        $data['db_data'] = $res;
-
-        $this->renderer->display('MangaCatalog', $data);
+        $this->renderer->display('Catalog', $data);
     }
 }
